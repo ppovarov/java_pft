a51181c613d4aba49d5e3ba57b965f766e17279a
Pavel Povarov <33931731+ppovarov@users.noreply.github.com>
2018-02-01 18:55:15 +0300

    REST integration with GitHub

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+    @Test
+    public void testCommits() throws IOException {
+        Github github = new RtGithub("25e74a2e34d55cdf3ed0ace0e306a0a7e137535d");
+        RepoCommits commits = github.repos().get(new Coordinates.Simple("ppovarov", "java_pft")).commits();
+        for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+            System.out.println(new RepoCommit.Smart(commit).message());
+        }
+    }
+}
